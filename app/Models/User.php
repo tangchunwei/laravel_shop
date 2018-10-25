38fe8c5605d092ab3cfdfc4823a8de921e52e936
tangchunwei <2239991414@qq.com>
2018-10-25 08:50:12 +0800

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,6 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password','email_verified',
     ];
@@ -21,12 +16,10 @@ class User extends Authenticatable
         'email_verified'=>'boolean',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function addresses(){
+        return $this->hasMany(UserAddress::class);
+    }
 }
