50ae5eef83d3a79a7585349964ee38365c26d19b
tangchunwei <2239991414@qq.com>
2018-10-30 15:37:10 +0800

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::group(['middleware'=>'auth'],function(){
         Route::delete('products/{product}/favorite','ProductsController@disfavor')->name('products.disfavor');
         // 商品收藏列表
         Route::get('products/favorites','ProductsController@favorites')->name('products.favorites');
+        // 添加购物车
+        Route::post('cart','CartController@add')->name('cart.add');
     });
 });
 // 测试
