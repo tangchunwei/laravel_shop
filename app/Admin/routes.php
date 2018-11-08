0728fbf61d3b0631190e2a9a171ff00522f38b75
tangchunwei <2239991414@qq.com>
2018-11-08 11:29:07 +0800

    '后台订单列表'

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -17,5 +17,5 @@ Route::group([
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
-
+    $router->get('orders', 'OrderController@index')->name('admin.orders.index');
 });
